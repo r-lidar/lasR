8ff94da4b30c2d9129ec5264b68dd806d66a7093
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2023-11-29 15:25:43 -0500

    fix test for windows-release github action

--- tests/testthat/test-pipelines.R
+++ tests/testthat/test-pipelines.R
@@ -9,6 +9,13 @@ test_that("buffer tiles",
   cont1 = processor(reader(f) + boundaries())
   cont2 = processor(reader(ans) + boundaries())
 
-  expect_equal(as.numeric(sum(sf::st_area(cont1))), 206788.110)
-  expect_equal(as.numeric(sum(sf::st_area(cont2))), 236796.375)
+  expect_equal(dim(cont1), c(4L,1L))
+
+  # Not using sf::st_area that fails on github action because of missing crs data.base
+  area = function(m) { diff(m[1:2,1]) * diff(m[2:3,2])}
+  area1 = 0; for (i in 1:4) area1 = area1 + area(cont1$geom[[i]][[1]])
+  area2 = 0; for (i in 1:4) area2 = area2 + area(cont2$geom[[i]][[1]])
+
+  expect_equal(area1, 206788.110)
+  expect_equal(area2, 236796.375)
 })
