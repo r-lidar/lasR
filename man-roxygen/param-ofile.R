403327e9bd8406791ea1e8b74aaf14b6293178de
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2023-12-05 09:15:38 -0500

    Doc ofile

--- man-roxygen/param-ofile.R
+++ man-roxygen/param-ofile.R
@@ -1,3 +1,4 @@
-#' @param ofile character. Outputs are always stored on disk. If \code{ofile = ""}
+#' @param ofile character. Full outputs are always stored on disk. If \code{ofile = ""}
 #' then the algorithm will not store the result on disk and will return nothing. It will however
-#' hold the results in memory. This is useful for algorithm that are only intermediate stage.
+#' hold partial output results temporarily in memory. This is useful for algorithm that are only
+#' intermediate stage.
