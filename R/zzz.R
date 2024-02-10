31017352184e5d15c2643ba8d1875ad2132838bc
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2024-02-10 06:55:55 -0500

    devel versioning

--- R/zzz.R
+++ R/zzz.R
@@ -41,36 +41,63 @@
 # Check if the package has more recent version
 check_update = function()
 {
+  msg <- NULL
+
+  last  <- get_latest_version()
+  curr <- utils::packageVersion("lasR")
+  new_version = !is.null(last) && last > curr
+  dev_version = is_dev_version(curr)
+
+  if (new_version)
+  {
+    if (dev_version)
+      msg = paste("lasR", last, "is now available. You are using", curr, "(unstable) \nremotes::install_github(\"r-lidar/lasR\")")
+    else
+      msg = paste("lasR", last, "is now available. You are using", curr, "\nremotes::install_github(\"r-lidar/lasR\")")
+  }
+  else if (dev_version)
+  {
+    msg = paste("lasR", curr, "is an unstable development version")
+  }
+
+  if (!is.null(msg)) packageStartupMessage(msg)
+  return(NULL)
+}
+
+get_latest_version = function()
+{
   f <- paste0(tempdir(), "/LASRDESCRIPTION")
 
   ans <- tryCatch(
-    {
-      nullcon <- file(nullfile(), open = "wb")
-      sink(nullcon, type = "message")
-      utils::download.file("https://raw.githubusercontent.com/r-lidar/lasR/main/DESCRIPTION", f)
-      sink(type = "message")
-      close(nullcon)
-      TRUE
-    },
-    error = function(e)
-    {
-      return(FALSE)
-    })
+  {
+    nullcon <- file(nullfile(), open = "wb")
+    sink(nullcon, type = "message")
+    utils::download.file("https://raw.githubusercontent.com/r-lidar/lasR/main/DESCRIPTION", f)
+    sink(type = "message")
+    close(nullcon)
+    TRUE
+  },
+  error = function(e)
+  {
+    return(FALSE)
+  })
 
   if (isFALSE(ans))
     return(NULL)
 
   m <- read.dcf(f)
-  dev <- m[4]
-  dev <- package_version(dev)
-  curr <- utils::packageVersion("lasR")
-  if (dev > curr)
-  {
-    packageStartupMessage(paste("lasR", dev, "is now available. You are using", curr, "\nremotes::install_github(\"r-lidar/lasR\")"))
-  }
-
-  return(NULL)
+  version <- m[4]
+  version <- package_version(version)
+  return(version)
 }
+
+is_dev_version = function(version)
+{
+  class(version) <- "list"
+  version = version[[1]]
+  return(length(version) == 4)
+}
+
 
 # ,@@@
 #  @@@@@@.@                                                           ,#*
