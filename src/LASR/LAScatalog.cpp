eb9aad3aaaa26b90c65005006ac84fa39c94de8e
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2024-03-05 21:59:47 -0500

    --

--- src/LASR/LAScatalog.cpp
+++ src/LASR/LAScatalog.cpp
@@ -423,7 +423,8 @@ bool LAScatalog::get_chunk(int i, Chunk& chunk) const
 
   if (!laskdtree->was_built())
   {
-    last_error = "internal error: spatial index of tile not built";
+    last_error = "internal error: spatial index of tile not built"; // # nocov
+    return false; // # nocov
   }
 
   bool success = false;
