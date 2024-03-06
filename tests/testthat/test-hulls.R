d8ec5e310beb3d578331e5bd6da04b120fbbef22
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2024-03-06 13:59:21 -0500

    Future tests

--- tests/testthat/test-hulls.R
+++ tests/testthat/test-hulls.R
@@ -25,3 +25,27 @@ test_that("hulls works with triangulation",
   expect_equal(dim(ans), c(1L,1L))
   expect_equal(length(ans$geom[[1]]), 5L) # there are outer and inner rings
 })
+
+test_that("hulls works with complex shapes",
+{
+  skip("Complex shapes for triangulation hulls not supported")
+
+  f <- system.file("extdata", "Megaplot.las", package="lasR")
+  read = reader(f)
+  del = triangulate(15, filter = keep_z_above(20), ofile = tempgpkg())
+  bound = lasR::hulls(del)
+  ans = processor(read+del+bound)
+
+  plot(ans$triangulate)
+  plot(ans$hulls)
+
+  f <- system.file("extdata", "Megaplot.las", package="lasR")
+  read = reader(f)
+  del = triangulate(-15, filter = keep_z_above(20), ofile = tempgpkg())
+  bound = lasR::hulls(del)
+  ans = processor(read+del+bound)
+
+  plot(ans$triangulate)
+  plot(ans$hulls)
+})
+
