5a3a386a4e6918da20da25442c9d7caf3843e563
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2024-03-11 08:13:15 -0400

    Fix tests

--- tests/testthat/test-queries.R
+++ tests/testthat/test-queries.R
@@ -20,18 +20,18 @@ test_that("reader_circle perform a queries",
   expect_equal(dim(ans), c(6368, 5L))
 
   # between two tiles with a buffer
-  pipeline = reader_las_circles(885150, 629400, 10, buffer = 5L) + read_las()
-  ans = exec(pipeline, f)
+  pipeline = reader_las_circles(885150, 629400, 10) + read_las()
+  ans = exec(pipeline, f, buffer = 5)
   expect_equal(dim(ans), c(14074, 5L))
   expect_equal(sum(ans$Buffer), 7706L)
 
   # between two tiles with a buffer but the centroid is not in a file
-  pipeline = reader_las_rectangles(885000L, 629390, 885040, 629410, buffer = 5) + read_las()
-  ans = exec(pipeline, f)
+  pipeline = reader_las_rectangles(885000L, 629390, 885040, 629410) + read_las()
+  ans = exec(pipeline, f, buffer = 5)
   expect_equal(dim(ans), c(5028L, 5L))
   expect_equal(sum(ans$Buffer), 2415L)
 
   # no match
-  pipeline = reader_las_circles(8850000, 629400, 20, buffer = 5) + read_las()
-  expect_error(exec(pipeline, f), "cannot find")
+  pipeline = reader_las_circles(8850000, 629400, 20) + read_las()
+  expect_error(exec(pipeline, f, buffer = 5), "cannot find")
 })
