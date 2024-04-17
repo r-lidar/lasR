664f4ba2e1144e49960d16d12579fc869fb4dc1e
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2024-04-17 03:15:35 -0400

    doc wording

--- man-roxygen/param-filter.R
+++ man-roxygen/param-filter.R
@@ -1,6 +1,6 @@
 #' @param filter the 'filter' argument allows filtering of the point-cloud to work with points of
 #' interest. The available filters are those from LASlib and can be found by running \link{filter_usage}.
-#' For a given algorithm when a filter is applied, only the points that meet the criteria are processes.
+#' For a given stage when a filter is applied, only the points that meet the criteria are processes.
 #' The most common strings are "-keep_first", "-keep_class 2", "drop_z_below 2". For more details see
 #' \link{filters}.
 #' @md
