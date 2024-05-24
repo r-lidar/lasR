c884bb8e3a598956b83f513be873071b841de8f4
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2024-05-24 11:14:08 -0400

    fix

--- src/LASRcore/LAScatalog.cpp
+++ src/LASRcore/LAScatalog.cpp
@@ -351,10 +351,10 @@ bool LAScatalog::write_vpc(const std::string& vpcfile, const CRS& crs, bool abso
     output << "      \"datetime\": " << autoquote(date) << "," << std::endl;
     output << "      \"pc:count\": " << n << "," << std::endl;;
     output << "      \"pc:type\": " << "\"lidar\"" << ","<< std::endl;
-    if (index) output << "      \"index:indexed\": " << "\"true\"," << std::endl;
+    if (index) output << "      \"index:indexed\": " << "true," << std::endl;
     output << "      \"proj:bbox\": [" << std::fixed << std::setprecision(3) << bbox.minx << ", " << bbox.miny << ", " << bbox.maxx << ", " << bbox.maxy << "],"<< std::endl;
     if (!wkt.empty()) output << "      \"proj:wtk2\": " << wkt << "," << std::endl;
-    if (epsg != 0) output << "      \"proj:epsg\": " << epsg << "," << std::endl;
+    if (epsg != 0) output << "      \"proj:epsg\": " << epsg << std::endl;
     output << "    }," << std::endl;
     output << "    \"links\": []," << std::endl;
     output << "    \"assets\": {" << std::endl;
