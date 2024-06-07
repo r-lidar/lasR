70f7e0059cacaff21b0377d0bdc34917b2869089
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2024-06-07 18:27:53 -0400

    unit tests

---
+++ tests/testthat/test-csf.R
@@ -0,0 +1,11 @@
+test_that("CSF works",
+{
+  f <- system.file("extdata", "Topography.las", package="lasR")
+  pipeline = classify_with_csf(TRUE, 1 ,1, time_step = 1) + summarise()
+  ans = exec(pipeline, on = f)
+  ans$npoints_per_class
+
+  class = c(226,45282,23998,3897)
+  names(class) = c(0,1,2,9)
+  expect_equal(ans$npoints_per_class, class, tolerance = 1)
+})
