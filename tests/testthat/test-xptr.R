fee0ff1a78f0dc9424cccf491ec8962d674d239d
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2024-11-20 17:35:28 -0500

    test

---
+++ tests/testthat/test-xptr.R
@@ -0,0 +1,20 @@
+test_that("external pointer works",
+{
+  f <- system.file("extdata", "Topography.las", package="lasR")
+
+  expect_error(ans <- lasR:::read_las(f), NA)
+
+
+  expect_equal(u$npoints, 73403)
+
+  exec(sampling_pixel(2), on = ans)
+
+  u = exec(summarise(), on = ans)
+  expect_equal(u$npoints, 17182)
+
+  u = exec(chm(5), on = ans)
+  expect_s4_class(u, "SpatRaster")
+
+  u = exec(local_maximum(5), on = ans)
+  expect_s3_class(u, "sf")
+})
