72b241ff59ad4e4f92a0f69a17a8c668074ecff5
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2024-12-12 14:20:57 -0500

    copc tests

--- tests/testthat/test-copc.R
+++ tests/testthat/test-copc.R
@@ -3,3 +3,45 @@ test_that("works with COPC",
   f = system.file("extdata", "example.copc.laz", package="lasR")
   expect_warning(exec(lasR:::nothing(read = T), f), NA)
 })
+
+test_that("Max depth in COPC",
+{
+  skip_if(Sys.info()[["user"]] != "jr")
+
+  f = "/home/jr/R packages/r-lidar/lasR/inst/extdata/autzen-classified.copc.laz"
+
+  pipeline = reader_las(filter = "-max_depth 0") + summarise()
+  ans = exec(pipeline, on = f)
+  expect_equal(ans$npoints, 61201L)
+
+  pipeline = reader_las(filter = "-max_depth 1") + summarise()
+  ans = exec(pipeline, on = f)
+  expect_equal(ans$npoints, 131060L)
+})
+
+test_that("Write COPC",
+{
+  skip_if(Sys.info()[["user"]] != "jr")
+
+  f = "/home/jr/R packages/r-lidar/lasR/inst/extdata/autzen-classified.copc.laz"
+  o = tempfile(fileext = ".copc.laz")
+
+  pipeline = reader_las(filter = "-max_depth 2") + write_las(o)
+  ans = exec(pipeline, on = f)
+
+  pipeline = reader_las(filter = "-max_depth 0") + summarise()
+  ans = exec(pipeline, on = o)
+  expect_equal(ans$npoints, 91876L, tolerance = 0.0001)
+
+  pipeline = reader_las(filter = "-max_depth 1") + summarise()
+  ans = exec(pipeline, on = o)
+  expect_equal(ans$npoints, 400000L,tolerance = 0.0001)
+
+  pipeline = reader_las(filter = "-max_depth 2") + summarise()
+  ans = exec(pipeline, on = o)
+  expect_equal(ans$npoints, 577637L)
+
+  pipeline = reader_las() + summarise()
+  ans = exec(pipeline, on = o)
+  expect_equal(ans$npoints, 577637L)
+})
