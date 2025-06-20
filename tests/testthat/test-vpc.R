6ac355f3df00c3ac3b3958859b1b995c01e8fb6e
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2025-06-20 18:56:48 -0400

    Fix tests on windows

--- tests/testthat/test-vpc.R
+++ tests/testthat/test-vpc.R
@@ -28,7 +28,8 @@ test_that("vpc writer works",
 
   ans = sf::st_read(ans, quiet = TRUE)
   expect_s3_class(ans, "sf")
-  expect_equal(dim(ans), c(4L,8L))
+  expect_equal(nrow(ans), 4L)
+  expect_true(all(sf::st_geometry_type(ans) == rep("POLYGON", 4)))
   expect_equal(as.numeric(sf::st_bbox(ans)), c(-127.627836098, 50.665469135, -127.624899322, 50.675051675))
 })
 
