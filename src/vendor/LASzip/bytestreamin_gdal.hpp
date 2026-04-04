c47216799994aba3a7968479d27b032b4cee34ab
Alex Grig <alex@devake.com>
2026-04-04 08:59:16 +0400

    fix: update authors in DESCRIPTION and bytestreamin_gdal.hpp

--- src/vendor/LASzip/bytestreamin_gdal.hpp
+++ src/vendor/LASzip/bytestreamin_gdal.hpp
@@ -11,11 +11,11 @@
 
   PROGRAMMERS:
 
-    jean-romain.roussel.1@ulaval.ca
+    agrigoriev@gmail.com
 
   COPYRIGHT:
 
-    (c) 2026, Jean-Romain Roussel
+    (c) 2026, Alexey Grigoryev
 
     This is free software; you can redistribute and/or modify it under the
     terms of the GNU Lesser General Licence as published by the Free Software
@@ -42,41 +42,43 @@
 class ByteStreamInGDAL : public ByteStreamIn
 {
 public:
-  ByteStreamInGDAL(VSILFILE* file);
-/* read a single byte                                        */
+  ByteStreamInGDAL(VSILFILE *file);
+  /* read a single byte                                        */
   U32 getByte();
-/* read an array of bytes                                    */
-  void getBytes(U8* bytes, const U32 num_bytes);
-/* is the stream seekable (e.g. stdin is not)                */
+  /* read an array of bytes                                    */
+  void getBytes(U8 *bytes, const U32 num_bytes);
+  /* is the stream seekable (e.g. stdin is not)                */
   BOOL isSeekable() const;
-/* get current position of stream                            */
+  /* get current position of stream                            */
   I64 tell() const;
-/* seek to this position in the stream                       */
+  /* seek to this position in the stream                       */
   BOOL seek(const I64 position);
-/* seek to the end of the file                               */
-  BOOL seekEnd(const I64 distance=0);
-/* destructor                                                */
+  /* seek to the end of the file                               */
+  BOOL seekEnd(const I64 distance = 0);
+  /* destructor                                                */
   ~ByteStreamInGDAL();
+
 protected:
-  VSILFILE* file;
+  VSILFILE *file;
 };
 
 class ByteStreamInGDALLE : public ByteStreamInGDAL
 {
 public:
-  ByteStreamInGDALLE(VSILFILE* file);
-/* read 16 bit low-endian field                              */
-  void get16bitsLE(U8* bytes);
-/* read 32 bit low-endian field                              */
-  void get32bitsLE(U8* bytes);
-/* read 64 bit low-endian field                              */
-  void get64bitsLE(U8* bytes);
-/* read 16 bit big-endian field                              */
-  void get16bitsBE(U8* bytes);
-/* read 32 bit big-endian field                              */
-  void get32bitsBE(U8* bytes);
-/* read 64 bit big-endian field                              */
-  void get64bitsBE(U8* bytes);
+  ByteStreamInGDALLE(VSILFILE *file);
+  /* read 16 bit low-endian field                              */
+  void get16bitsLE(U8 *bytes);
+  /* read 32 bit low-endian field                              */
+  void get32bitsLE(U8 *bytes);
+  /* read 64 bit low-endian field                              */
+  void get64bitsLE(U8 *bytes);
+  /* read 16 bit big-endian field                              */
+  void get16bitsBE(U8 *bytes);
+  /* read 32 bit big-endian field                              */
+  void get32bitsBE(U8 *bytes);
+  /* read 64 bit big-endian field                              */
+  void get64bitsBE(U8 *bytes);
+
 private:
   U8 swapped[8];
 };
@@ -84,24 +86,25 @@ private:
 class ByteStreamInGDALBE : public ByteStreamInGDAL
 {
 public:
-  ByteStreamInGDALBE(VSILFILE* file);
-/* read 16 bit low-endian field                              */
-  void get16bitsLE(U8* bytes);
-/* read 32 bit low-endian field                              */
-  void get32bitsLE(U8* bytes);
-/* read 64 bit low-endian field                              */
-  void get64bitsLE(U8* bytes);
-/* read 16 bit big-endian field                              */
-  void get16bitsBE(U8* bytes);
-/* read 32 bit big-endian field                              */
-  void get32bitsBE(U8* bytes);
-/* read 64 bit big-endian field                              */
-  void get64bitsBE(U8* bytes);
+  ByteStreamInGDALBE(VSILFILE *file);
+  /* read 16 bit low-endian field                              */
+  void get16bitsLE(U8 *bytes);
+  /* read 32 bit low-endian field                              */
+  void get32bitsLE(U8 *bytes);
+  /* read 64 bit low-endian field                              */
+  void get64bitsLE(U8 *bytes);
+  /* read 16 bit big-endian field                              */
+  void get16bitsBE(U8 *bytes);
+  /* read 32 bit big-endian field                              */
+  void get32bitsBE(U8 *bytes);
+  /* read 64 bit big-endian field                              */
+  void get64bitsBE(U8 *bytes);
+
 private:
   U8 swapped[8];
 };
 
-inline ByteStreamInGDAL::ByteStreamInGDAL(VSILFILE* file)
+inline ByteStreamInGDAL::ByteStreamInGDAL(VSILFILE *file)
 {
   this->file = file;
 }
@@ -128,7 +131,7 @@ inline U32 ByteStreamInGDAL::getByte()
   return (U32)byte;
 }
 
-inline void ByteStreamInGDAL::getBytes(U8* bytes, const U32 num_bytes)
+inline void ByteStreamInGDAL::getBytes(U8 *bytes, const U32 num_bytes)
 {
   if (VSIFReadL(bytes, 1, num_bytes, file) != num_bytes)
   {
@@ -161,33 +164,33 @@ inline BOOL ByteStreamInGDAL::seekEnd(const I64 distance)
   return !(VSIFSeekL(file, (vsi_l_offset)(-(I64)distance), SEEK_END));
 }
 
-inline ByteStreamInGDALLE::ByteStreamInGDALLE(VSILFILE* file) : ByteStreamInGDAL(file)
+inline ByteStreamInGDALLE::ByteStreamInGDALLE(VSILFILE *file) : ByteStreamInGDAL(file)
 {
 }
 
-inline void ByteStreamInGDALLE::get16bitsLE(U8* bytes)
+inline void ByteStreamInGDALLE::get16bitsLE(U8 *bytes)
 {
   getBytes(bytes, 2);
 }
 
-inline void ByteStreamInGDALLE::get32bitsLE(U8* bytes)
+inline void ByteStreamInGDALLE::get32bitsLE(U8 *bytes)
 {
   getBytes(bytes, 4);
 }
 
-inline void ByteStreamInGDALLE::get64bitsLE(U8* bytes)
+inline void ByteStreamInGDALLE::get64bitsLE(U8 *bytes)
 {
   getBytes(bytes, 8);
 }
 
-inline void ByteStreamInGDALLE::get16bitsBE(U8* bytes)
+inline void ByteStreamInGDALLE::get16bitsBE(U8 *bytes)
 {
   getBytes(swapped, 2);
   bytes[0] = swapped[1];
   bytes[1] = swapped[0];
 }
 
-inline void ByteStreamInGDALLE::get32bitsBE(U8* bytes)
+inline void ByteStreamInGDALLE::get32bitsBE(U8 *bytes)
 {
   getBytes(swapped, 4);
   bytes[0] = swapped[3];
@@ -196,7 +199,7 @@ inline void ByteStreamInGDALLE::get32bitsBE(U8* bytes)
   bytes[3] = swapped[0];
 }
 
-inline void ByteStreamInGDALLE::get64bitsBE(U8* bytes)
+inline void ByteStreamInGDALLE::get64bitsBE(U8 *bytes)
 {
   getBytes(swapped, 8);
   bytes[0] = swapped[7];
@@ -209,18 +212,18 @@ inline void ByteStreamInGDALLE::get64bitsBE(U8* bytes)
   bytes[7] = swapped[0];
 }
 
-inline ByteStreamInGDALBE::ByteStreamInGDALBE(VSILFILE* file) : ByteStreamInGDAL(file)
+inline ByteStreamInGDALBE::ByteStreamInGDALBE(VSILFILE *file) : ByteStreamInGDAL(file)
 {
 }
 
-inline void ByteStreamInGDALBE::get16bitsLE(U8* bytes)
+inline void ByteStreamInGDALBE::get16bitsLE(U8 *bytes)
 {
   getBytes(swapped, 2);
   bytes[0] = swapped[1];
   bytes[1] = swapped[0];
 }
 
-inline void ByteStreamInGDALBE::get32bitsLE(U8* bytes)
+inline void ByteStreamInGDALBE::get32bitsLE(U8 *bytes)
 {
   getBytes(swapped, 4);
   bytes[0] = swapped[3];
@@ -229,7 +232,7 @@ inline void ByteStreamInGDALBE::get32bitsLE(U8* bytes)
   bytes[3] = swapped[0];
 }
 
-inline void ByteStreamInGDALBE::get64bitsLE(U8* bytes)
+inline void ByteStreamInGDALBE::get64bitsLE(U8 *bytes)
 {
   getBytes(swapped, 8);
   bytes[0] = swapped[7];
@@ -242,17 +245,17 @@ inline void ByteStreamInGDALBE::get64bitsLE(U8* bytes)
   bytes[7] = swapped[0];
 }
 
-inline void ByteStreamInGDALBE::get16bitsBE(U8* bytes)
+inline void ByteStreamInGDALBE::get16bitsBE(U8 *bytes)
 {
   getBytes(bytes, 2);
 }
 
-inline void ByteStreamInGDALBE::get32bitsBE(U8* bytes)
+inline void ByteStreamInGDALBE::get32bitsBE(U8 *bytes)
 {
   getBytes(bytes, 4);
 }
 
-inline void ByteStreamInGDALBE::get64bitsBE(U8* bytes)
+inline void ByteStreamInGDALBE::get64bitsBE(U8 *bytes)
 {
   getBytes(bytes, 8);
 }
