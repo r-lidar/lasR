658b499e257679d4e8f553efa1210782e88a6198
Alex Grioryev <alex@devake.com>
2026-05-01 08:49:42 +0800

    fix: coerce callback formals to list

--- R/stages.R
+++ R/stages.R
@@ -182,7 +182,7 @@ aggregate_q = function(res, call, filter, ofile, env, ...)
 callback = function(fun, expose = "xyz", ..., drop_buffer = FALSE, no_las_update = FALSE)
 {
   args <- list(...)
-  fargs <- formals(fun)
+  fargs <- as.list(formals(fun))
   fargs <- fargs[-1]
   fargs[names(args)] <- args
 
@@ -1563,4 +1563,3 @@ LASATTRIBUTES <- c("X", "Y", "Z", "Intensity",
                    "ScannerChannel", "NIR",
                    "UserData", "gpstime", "PointSourceID",
                    "R", "G", "B")
-
